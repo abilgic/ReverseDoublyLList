9fb66455984eb927979ce1e264c5323328280044
ahmet bilgic <ahmet_bilgic@yahoo.com>
2023-07-11 19:53:25 +0300

    Add project files.

---
+++ DLList.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseDoublyLList
+{
+
+    public class DLList
+    {
+        public Node head;
+        public class Node
+        {
+            public int Data;
+            public Node Prev;
+            public Node Next;
+            public Node() { }
+            public Node(int data)
+            {
+                Data = data;
+            }
+
+
+        }
+        public void insert(int data)
+        {
+            if (head == null)
+            {
+
+                head = new Node(data);
+
+                return;
+            }
+
+            var current = head;
+
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+
+            var newnode = new Node(data);
+
+            current.Next = newnode;
+
+            newnode.Prev = current;
+            newnode.Next = null;
+
+
+
+        }
+
+        public void Print()
+        {
+            if (head == null)
+                return;
+            var current = head;
+            while (current.Next != null)
+            {
+                Console.Write($"{current.Data} ");
+                current = current.Next;
+            };
+        }
+    }
+}
+
+
